82bb77f91c163c2472f883f5b4b4141ccde0e5ad
lcs2001 <lcs2001_@hotmail.com>
2021-01-22 19:37:50 -0300

    Mais tipos de verificacao usando Mockito

--- src/test/java/org/lcs2001/MockitoTests.java
+++ src/test/java/org/lcs2001/MockitoTests.java
@@ -2,17 +2,23 @@ package org.lcs2001;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RunWith (MockitoJUnitRunner.class) // Para o @Mock funcionar, classe deve rodar com esse Runner do Mockito
 public class MockitoTests {
+
+    @Mock
+    List<String> lista; // Instância de lista Mockito
 
     @Test
     public void primeiroTesteMockito() {
-        List<String> lista = Mockito.mock(ArrayList.class); // Instância uma lista utilizando o Mockito
-
         Mockito.when( lista.size() ).thenReturn(20); // Toda return do tamanho dessa instância de lista mockito retorna que possui 20 elementos
 
         int size = lista.size();
@@ -22,14 +28,38 @@ public class MockitoTests {
 
     @Test
     public void segundoTesteMockito() {
-        List<String> lista = Mockito.mock(ArrayList.class);
-        lista.add("Elemento 0");
-        lista.add("Elemento 1");
+        List<String> list = Mockito.mock(ArrayList.class); // Outra forma de declarar uma lista Mockito
+        list.add("Elemento 0");
+        list.add("Elemento 1");
 
         // Mockito.when( lista.size() ).thenReturn(20);
 
-        int size = lista.size(); // Vai retornar zero pois se não altera o tamanho pelo Mockito, não muda
+        int size = list.size(); // Vai retornar zero pois se não altera o tamanho pelo Mockito, não muda
 
         Assertions.assertThat(size).isEqualTo(0);
+    }
+
+    @Test
+    public void terceiroTesteMockito() {
+        List<String> list = Mockito.mock(ArrayList.class);
+
+        list.size();
+
+        Mockito.verify(list, Mockito.times(1)).size(); // verify() para verificar se já foi utilizado o .size()
+        Mockito.verify(list, Mockito.never()).isEmpty(); // verify() para verificar se já foi utilizado o .size()
+    }
+
+    @Test
+    public void quartoTesteMockito() {
+        List<String> list = Mockito.mock(ArrayList.class);
+
+        // Se alterar a ordem desses dois comandos, retorna erro
+        list.size();
+        list.add("");
+
+        // Verificar se esses comandos foram executados exatamente nessa ordem para essa lista
+        InOrder inOrder = Mockito.inOrder(list);
+        inOrder.verify(list).size();
+        inOrder.verify(list).add("");
     }
 }
