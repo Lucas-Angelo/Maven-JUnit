44d95679db3b6fc08b6964144fe771a5990c24e4
lcs2001 <lcs2001_@hotmail.com>
2021-01-22 11:12:32 -0300

    Calculadora testada com AssertJ

--- src/main/java/org/lcs2001/PrimeiroTeste.java
+++ src/main/java/org/lcs2001/PrimeiroTeste.java
@@ -9,10 +9,11 @@ public class PrimeiroTeste {
     @Test
     public void deveSomar2Numeros() {
         // cenário
+        Calculadora calculadora = new Calculadora();
         int numero1=10, numero2=5;
 
         // execução
-        int resultado = numero1 + numero2;
+        double resultado = calculadora.somar(numero1, numero2);
 
         // verificações
         // Assert.assertEquals(15, resultado); // Assertiva do JUnit
@@ -27,6 +28,63 @@ public class PrimeiroTeste {
         Assertions.assertThat("").isEmpty();
         Assertions.assertThat("Lucas").isGreaterThan("Lcs");
         */
-
     }
+
+    @Test
+    public void deveSubtrair2Numeros() {
+        // cenário
+        Calculadora calculadora = new Calculadora();
+        int numero1=-15, numero2=5;
+
+        // execução
+        double resultado = calculadora.subtrair(numero1, numero2);
+
+        // verificações
+        Assertions.assertThat(resultado).isEqualTo(-20);
+    }
+
+    @Test
+    public void deveMultiplicar2Numeros() {
+        // cenário
+        Calculadora calculadora = new Calculadora();
+        int numero1=5, numero2=-5;
+
+        // execução
+        double resultado = calculadora.multiplicar(numero1, numero2);
+
+        // verificações
+        Assertions.assertThat(resultado).isEqualTo(-25);
+    }
+
+    @Test (expected = RuntimeException.class) // Para não dar erro, já se é esperado retornar uma execessão de dividir por zero
+    public void naoDeveDividirPorZero() {
+        // cenário
+        Calculadora calculadora = new Calculadora();
+        int numero1=10, numero2=0;
+
+        calculadora.dividir(numero1, numero2);
+    }
+}
+
+class Calculadora {
+
+    double somar(double num1, double num2) {
+            return num1+num2;
+    }
+
+    double subtrair(double num1, double num2) {
+        return num1-num2;
+    }
+
+    double multiplicar(double num1, double num2) {
+        return num1*num2;
+    }
+
+    double dividir(double num1, double num2) {
+        if(num2==0)
+            throw new RuntimeException("Não é permitido dividir por zero!");
+        else
+            return num1/num2;
+    }
+
 }
