54ca390395db04c5cb7f4739fac660174d78d774
lcs2001 <lcs2001_@hotmail.com>
2021-01-22 09:16:55 -0300

    Biblioteca de teste JUnit

--- src/main/java/org/lcs2001/PrimeiroTeste.java
+++ src/main/java/org/lcs2001/PrimeiroTeste.java
@@ -1,7 +1,8 @@
 package org.lcs2001;
 
-import org.junit.Assert;
+// import org.junit.Assert; // Essa é a Assertiva do JUnit, não tão completa
 import org.junit.Test;
+import org.assertj.core.api.Assertions;
 
 public class PrimeiroTeste {
 
@@ -14,8 +15,14 @@ public class PrimeiroTeste {
         int resultado = numero1 + numero2;
 
         // verificações
-        Assert.assertEquals(15, resultado);
+        // Assert.assertEquals(15, resultado); // Assertiva do JUnit
+        Assertions.assertThat(resultado).isBetween(14, 16);
+        Assertions.assertThat(resultado).isEqualTo(15);
+        Assertions.assertThat(resultado).isGreaterThan(10);
+        Assertions.assertThat(resultado).isLessThan(20);
 
+        Assertions.assertThat("").isEmpty();
+        Assertions.assertThat("Lucas").isGreaterThan("Lcs");
 
     }
 }
